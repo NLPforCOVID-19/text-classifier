9a9852a7d6d3008b6ff66728c7293c4c8d4ce65e
christopher <christopher@orudo.cc>
2020-04-16 16:24:10 +0900

    update uploader

--- covid-19-annotation-cleanup/uploader.py
+++ covid-19-annotation-cleanup/uploader.py
@@ -2,9 +2,7 @@ from doccano_api_client import DoccanoClient
 
 # instantiate a client and log in to a Doccano instance
 doccano_client = DoccanoClient(
-    'http://54.199.70.161/',
-    'christopher',
-    'cjj8120972'
+    'http://54.199.70.161/'
 )
 
 with open("data/uploadlist") as f:
