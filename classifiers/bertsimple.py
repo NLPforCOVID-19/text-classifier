312060f88e3d69bc0d2b0245c7845387c88e1ed6
yusuke <yusuke@is.s.u-tokyo.ac.jp>
2020-06-25 11:29:12 +0900

    compute learning curve

--- classifiers/bertsimple.py
+++ classifiers/bertsimple.py
@@ -102,6 +102,87 @@ def make_batch(features):
     mask = torch.stack([torch.tensor([i < sentences.shape[0] for i in range(max_len)]) for sentences in features])
     return batch, mask
 
+def learning_curve_mode(args, classes):
+    """Learning curve mode"""
+    logger.info("Learning curve mode")
+
+    # initialize BERT models
+    logger.info("Loading BERT model: %s", args.bert_model)
+    bert, tokenizer = init_bert(args.bert_model)
+    bert.eval()
+    bert.to(args.device)
+
+    # run BERT on text file to compute document embeddings
+    features, labels = run_bert(args.text_file, classes, bert, tokenizer, batch_size=args.batch, device=args.device)
+    if len(features) != len(labels):
+        raise ValueError("Train mode requires labeled text data")
+    train_all_features, val_features, train_all_labels, val_labels = train_test_split(features, labels, test_size=0.1, shuffle=True, random_state=100)
+    num_all_train = len(train_all_labels)
+    num_all_labels = sum(train_all_labels)
+    logger.info("Num train: %s", num_all_train)
+    logger.info("Labels: %s", ' '.join([f"{label}:{int(num)}" for label, num in zip(classes, num_all_labels)]))
+    
+    # train model varying data size
+    dataset_indices = torch.randperm(num_all_train)
+    for dataset_ratio in args.dataset_ratio:
+        logger.info("Start training: %s", dataset_ratio)
+        num_train = int(num_all_train * dataset_ratio)
+        train_features = [train_all_features[i] for i in dataset_indices[:num_train]]
+        train_labels = [train_all_labels[i] for i in dataset_indices[:num_train]]
+        num_labels = sum(train_labels)
+        classifier = init_classifier(train_features[0].shape[1], len(classes))
+        classifier.to(args.device)
+        if args.pos_weight:
+            # weighting positive instances to deal with imbalanced labels (weight = # negative / # positive)
+            pos_weight = torch.tensor([float(num_train - pos) / pos for pos in num_labels]).to(args.device)
+            #print(pos_weight)
+            criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        else:
+            # no pos_weight
+            criterion = nn.BCEWithLogitsLoss()
+        optimizer = optim.Adagrad(classifier.parameters())
+        # start training
+        for epoch in range(args.epoch):
+            total_loss = 0.0
+            permutation = torch.randperm(num_train)
+            for i in range(0, num_train, args.batch):  # run classifier on minibatch
+                indices = permutation[i:i+args.batch]
+                batch, mask = make_batch([train_features[i] for i in indices])
+                batch = batch.to(args.device)
+                mask = mask.to(args.device)
+                gold = torch.stack([train_labels[i] for i in indices]).to(args.device)
+                optimizer.zero_grad()
+                output = classifier(batch, mask)   # class scores
+                #print(output)
+                #print(gold)
+                loss = criterion(output, gold)
+                loss.backward()
+                optimizer.step()
+                total_loss += float(loss.item())
+            total_loss /= (num_train // args.batch + 1)
+            logger.info(f"Epoch {epoch+1}: loss={total_loss}")
+            # compute validation loss and accuracy
+            with torch.no_grad():
+                val_loss = 0
+                val_probs = []
+                for i in range(0, len(val_features), args.batch):
+                    val_batch, val_mask = make_batch(val_features[i:i+args.batch])
+                    val_batch = val_batch.to(args.device)
+                    val_mask = val_mask.to(args.device)
+                    val_gold = torch.stack(val_labels[i:i+args.batch]).to(args.device)
+                    val_output = classifier(val_batch, val_mask)
+                    val_loss += float(criterion(val_output, val_gold).item())
+                    val_probs.append(torch.sigmoid(val_output).cpu())
+                val_loss /= (len(val_features) / args.batch + 1)
+            prec, rec, fscore, _ = precision_recall_fscore_support(torch.stack(val_labels), (torch.cat(val_probs) > 0.5).to(dtype=float), zero_division=0)
+            acc = ' '.join([f"{cl}={p:.3f}/{r:.3f}/{f:.3f}" for cl, p, r, f in zip(classes, prec, rec, fscore)])
+            logger.info(f"val_loss={val_loss} acc: {acc}")
+        logger.info("done")
+
+        # output final accuracy
+        logger.info("t=%s: %s", dataset_ratio, ' '.join([f"{cl}={p:.3f}/{r:.3f}/{f:.3f}" for cl, p, r, f in zip(classes, prec, rec, fscore)]))
+    return
+
 def train_mode(args, classes):
     """Train mode"""
     logger.info("Train mode")
@@ -223,7 +304,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s: %(message)s')
     
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("mode", choices=["train", "test"], help="Run mode")
+    argparser.add_argument("mode", choices=["train", "test", "learning"], help="Run mode")
     argparser.add_argument("--bert-model", required=True, help="Path to BERT model")
     argparser.add_argument("--model-path", required=True, help="Path to the classifier mdoel")
     argparser.add_argument("--text-file", required=True, help="Text file (JSONL)")
@@ -233,6 +314,7 @@ if __name__ == "__main__":
     argparser.add_argument("--batch", type=int, default=128, help="Batch size")
     argparser.add_argument("--epoch", type=int, default=30, help="Number of epochs")
     argparser.add_argument("--pos-weight", action="store_true", help="Use pos_weight in loss function")
+    argparser.add_argument("--dataset-ratio", type=lambda x: list(map(float, x.split(','))), help="Ratio of data used for training (comma-separated float values; only valid for learning curve mode)")
     args = argparser.parse_args()
     if args.gpu and torch.cuda.is_available():
         logger.info("Using GPU")
@@ -248,6 +330,11 @@ if __name__ == "__main__":
     # start train or test
     if args.mode == "train":
         train_mode(args, classes)
+    elif args.mode == "learning":
+        if args.dataset_ratio is None:
+            logger.error("Learning curve mode requires --dataset-ratio")
+            sys.exit(1)
+        learning_curve_mode(args, classes)
     else:
         assert args.mode == "test"
         if args.output_file is None:
