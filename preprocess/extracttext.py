9d5c6e6e8632b68482ba71302eaf0eb3c1539463
yusuke <yusuke@is.s.u-tokyo.ac.jp>
2020-07-14 15:32:18 +0900

    support multiprocess

--- preprocess/extracttext.py
+++ preprocess/extracttext.py
@@ -8,8 +8,12 @@ import json
 from xml.etree import ElementTree
 from pyknp import Juman
 from collections import defaultdict
+import multiprocessing
+import functools
 
-def to_text(juman, standard_format):
+logger = logging.getLogger(__name__)
+
+def to_text(juman, etree):
     "Convert standard format into a simple text (list of word lists)"
     texts = etree.findall(".//Title") + etree.findall(".//Description") + etree.findall(".//S")
     wordlists, poslists, rawsentences = [], [], []
@@ -23,6 +27,25 @@ def to_text(juman, standard_format):
         rawsentences.append(rawsentence.text)
     return wordlists, poslists, rawsentences
 
+def extract_text(line, target, directory):
+    line = line.strip()
+    page = json.loads(line)
+    if target not in page or "xml_file" not in page[target]: return None
+    xml_file = page[target]["xml_file"]
+    #print(xml_file)
+    try:
+        etree = ElementTree.parse(f"{directory}/{xml_file}")
+    except ElementTree.ParseError:
+        logger.error("XML file broken: %s", xml_file)
+        return None
+    wordlists, poslists, rawsentences = to_text(juman, etree)
+
+    # output the results into JSONL file
+    page["rawsentences"] = rawsentences
+    page["wordlists"] = wordlists
+    page["poslists"] = poslists
+    return page
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s: %(message)s')
     
@@ -31,32 +54,26 @@ if __name__ == "__main__":
     argparser.add_argument("-t", "--target", default="ja_translated", help="JSON attribute of target language")
     argparser.add_argument("metadata_file", help="Metadata file (JSONL)")
     argparser.add_argument("output_file", help="Output file (JSONL)")
+    argparser.add_argument("-j", "--num-processes", default=1, type=int, help="Number of processes to run")
     args = argparser.parse_args()
 
     # initialize juman (used only for parsing standard format)
     juman = Juman("cat")  # use dummy command to avoid installing jumanpp...
 
-    num_pages = 0
-    num_ignored = 0
-    statistics = defaultdict(int)
     with open(args.metadata_file, "r") as metadata_file, open(args.output_file, "w") as of:
-        for line in metadata_file:
-            line = line.strip()
-            page = json.loads(line)
-            xml_file = page[args.target]["xml_file"]
-            #print(xml_file)
-            try:
-                etree = ElementTree.parse(f"{args.directory}/{xml_file}")
-                num_pages += 1
-            except ElementTree.ParseError:
-                logger.error("XML file broken: %s", xml_file)
-                num_ignored += 1
-                continue
-            wordlists, poslists, rawsentences = to_text(juman, etree)
+        if args.num_processes == 1:
+            for line in metadata_file:
+                page = extract_text(line, args.target, args.directory)
+                if page is not None:
+                    json.dump(page, of, ensure_ascii=False)
+                    of.write("\n")
+        else:
+            # run multiprocessing
+            extract_text_func = functools.partial(extract_text, target=args.target, directory=args.directory)
+            with multiprocessing.Pool(args.num_processes) as pool:
+                for page in pool.imap(extract_text_func, metadata_file, chunksize=10):
+                    if page is not None:
+                        json.dump(page, of, ensure_ascii=False)
+                        of.write("\n")
 
-            # output the results into JSONL file
-            page["rawsentences"] = rawsentences
-            page["wordlists"] = wordlists
-            page["poslists"] = poslists
-            json.dump(page, of, ensure_ascii=False)
-            of.write("\n")
+                    
--- preprocess/metadata.py
+++ preprocess/metadata.py
@@ -12,7 +12,11 @@ import bs4
 import json
 import datetime
 from collections import defaultdict
+import multiprocessing
+import functools
+import logging
 
+logger = logging.getLogger(__name__)
 
 def extract_url_from_url_file(filepath: pathlib.Path) -> str:
     with filepath.open() as f:
@@ -21,67 +25,87 @@ def extract_url_from_url_file(filepath: pathlib.Path) -> str:
 
 def extract_title_from_html_file(filepath: pathlib.Path) -> str:
     with filepath.open() as f:
-        title = bs4.BeautifulSoup(f.read(), "html.parser").title
+        try:
+            title = bs4.BeautifulSoup(f.read(), "html.parser").title
+        except Exception:
+            # somehow HTML parsing failed
+            return ""
         if title is None:
             return ""
         else:
-            return title.string
+            return str(title.string)
 
 
 def extract_timestamp_from_file(filepath: pathlib.Path) -> str:
     return datetime.datetime.fromtimestamp(os.path.getmtime(str(filepath))).isoformat()
 
 
+def extract_metadata(line: str, directory: str) -> dict:
+    # decompose a url, which is like `./html/<country>/orig/<domain>/<filename>`, into its parts
+    _, country, _, domain, *url_parts = pathlib.Path(line.strip()).parts
+    url_filename = pathlib.Path(*url_parts)
+
+    # list file paths
+    data_dir = pathlib.Path(directory)
+    url_filepath = data_dir / "html" / country / "orig" / domain / url_filename.with_suffix(".url")
+    orig_filepath = data_dir / "html" / country / "orig" / domain / url_filename.with_suffix(".html")
+    ja_filepath = data_dir / "html" / country / "ja_translated" / domain / url_filename.with_suffix(".html")
+    xml_filepath = data_dir / "xml" / country / "ja_translated" / domain / url_filename.with_suffix(".xml")
+
+    # extract metadata by reading the files
+    orig_url = extract_url_from_url_file(url_filepath)
+    orig_title = extract_title_from_html_file(orig_filepath)
+    orig_timestamp = extract_timestamp_from_file(orig_filepath)
+    meta = {
+        "country": country,
+        "orig": {
+            "file": str(orig_filepath.relative_to(data_dir)),
+            "title": orig_title,
+            "timestamp": orig_timestamp
+        },
+        "url": orig_url,
+        "domain": domain
+    }
+
+    # extract metadata of ja_translated
+    if ja_filepath.exists():
+        ja_title = extract_title_from_html_file(ja_filepath)
+        ja_timestamp = extract_timestamp_from_file(ja_filepath)
+        meta["ja_translated"] = {
+            "file": str(ja_filepath.relative_to(data_dir)),
+            "title": ja_title,
+            "timestamp": ja_timestamp,
+        }
+        if xml_filepath.exists():
+            xml_timestamp = extract_timestamp_from_file(xml_filepath)
+            meta["ja_translated"].update({
+                "xml_file": str(xml_filepath.relative_to(data_dir)),
+                "xml_timestamp": xml_timestamp
+            })
+
+    return meta
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("-d", "--directory", default=".", help="Path prefix of URL files")
     argparser.add_argument("url_files", help="File paths of input URL files (*.url; one path per line)")
     argparser.add_argument("output_file", help="Output file (JSONL)")
+    argparser.add_argument("-j", "--num-processes", default=1, type=int, help="Number of processes to run")
     args = argparser.parse_args()
+    logger.info(args)
 
     with open(args.url_files, "r") as f, open(args.output_file, "w") as of:
-        for line in f:
-            # decompose a url, which is like `./html/<country>/orig/<domain>/<filename>`, into its parts
-            _, country, _, domain, *url_parts = pathlib.Path(line.strip()).parts
-            url_filename = pathlib.Path(*url_parts)
+        # output the metadata as a JSONL file
+        if args.num_processes == 1:
+            for line in f:
+                json.dump(extract_metadata(line, args.directory), of, ensure_ascii=False)
+                of.write("\n")
+        else:
+            # run multiprocessing
+            extract_metadata_func = functools.partial(extract_metadata, directory=args.directory)
+            with multiprocessing.Pool(args.num_processes) as pool:
+                for meta in pool.imap(extract_metadata_func, f, chunksize=10):
+                    json.dump(meta, of, ensure_ascii=False)
+                    of.write("\n")
 
-            # list file paths
-            data_dir = pathlib.Path(args.directory)
-            url_filepath = data_dir / "html" / country / "orig" / domain / url_filename.with_suffix(".url")
-            orig_filepath = data_dir / "html" / country / "orig" / domain / url_filename.with_suffix(".html")
-            ja_filepath = data_dir / "html" / country / "ja_translated" / domain / url_filename.with_suffix(".html")
-            xml_filepath = data_dir / "xml" / country / "ja_translated" / domain / url_filename.with_suffix(".xml")
-
-            # extract metadata by reading the files
-            orig_url = extract_url_from_url_file(url_filepath)
-
-            orig_title = extract_title_from_html_file(orig_filepath)
-            ja_title = extract_title_from_html_file(ja_filepath)
-
-            orig_timestamp = extract_timestamp_from_file(orig_filepath)
-            ja_timestamp = extract_timestamp_from_file(ja_filepath)
-            xml_timestamp = extract_timestamp_from_file(xml_filepath)
-
-            # append the metadata
-            meta = {
-                "country": country,
-                "orig": {
-                    "file": str(orig_filepath.relative_to(data_dir)),
-                    "title": orig_title,
-                    "timestamp": orig_timestamp
-                },
-                "ja_translated": {
-                    "file": str(ja_filepath.relative_to(data_dir)),
-                    "title": ja_title,
-                    "timestamp": ja_timestamp,
-                    "xml_file": str(xml_filepath.relative_to(data_dir)),
-                    "xml_timestamp": xml_timestamp
-                },
-                "url": orig_url,
-                "domain": domain
-            }
-            
-            # output the metadata as a JSONL file
-            json.dump(meta, of, ensure_ascii=False)
-            of.write("\n")
 
